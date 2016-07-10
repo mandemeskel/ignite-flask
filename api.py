4b6d1d13df2868a243a4d822172dc08076c21e1f
Michael T. Andemeskel <mtandemeskel@gmail.com>
2016-07-09 23:00:36 -0700

    created models, needs to decide to use Search API or just models

--- api.py
+++ api.py
@@ -17,3 +17,245 @@ from crossdomain import crossdomain
 
 app = Flask(__name__)
 app.debug = True
+
+
+@app.route( "/api/topics", methods=["GET"] )
+def get_topics():
+	pass
+
+
+
+# Topic model handlers
+'''
+Handles DELETE request
+'''
+@app.route( "/api/topic/<topic>", methods=["DELETE"] )
+# @crossdomain(origin='*',  methods=["DELETE"])
+def delete_topic( topic ):
+	pass
+
+
+'''
+Handles GET request
+'''
+@app.route( "/api/topic/<topic>", methods=["GET"] )
+# @crossdomain(origin='*',  methods=["GET"])
+def get_topic( topic ):
+	pass
+
+
+'''
+Handles OPTIONS request
+'''
+@app.route( "/api/topic/<topic>", methods=["OPTIONS"] )
+# @crossdomain(origin='*',  methods=["OPTIONS"])
+def options_topic( topic ):
+	pass
+
+
+'''
+Handles POST request
+'''
+@app.route( "/api/topic/<topic>", methods=["POST"] )
+# @crossdomain(origin='*',  methods=["POST"])
+def post_topic( topic ):
+	pass
+
+
+'''
+Handles PUT request
+'''
+@app.route( "/api/topic/<topic>", methods=["PUT"] )
+# @crossdomain(origin='*',  methods=["PUT"])
+def update_topic( topic ):
+	pass
+
+
+
+# Sub-Topic 
+'''
+Handles DELETE request
+'''
+@app.route( "/api/subtopic/<subtopic>", methods=["DELETE"] )
+# @crossdomain(origin='*',  methods=["DELETE"])
+def delete_topic( topic ):
+	pass
+
+
+'''
+Handles GET request
+'''
+@app.route( "/api/subtopic/<subtopic>", methods=["GET"] )
+# @crossdomain(origin='*',  methods=["GET"])
+def get_topic( topic ):
+	pass
+
+
+'''
+Handles OPTIONS request
+'''
+@app.route( "/api/subtopic/<subtopic>", methods=["OPTIONS"] )
+# @crossdomain(origin='*',  methods=["OPTIONS"])
+def options_topic( topic ):
+	pass
+
+
+'''
+Handles POST request
+'''
+@app.route( "/api/subtopic/<subtopic>", methods=["POST"] )
+# @crossdomain(origin='*',  methods=["POST"])
+def post_topic( topic ):
+	pass
+
+
+'''
+Handles PUT request
+'''
+@app.route( "/api/subtopic/<subtopic>", methods=["PUT"] )
+# @crossdomain(origin='*',  methods=["PUT"])
+def update_topic( topic ):
+	pass
+
+
+
+# Resource model handlers
+'''
+Handles DELETE request
+'''
+@app.route( "/api/resource/<resource>", methods=["DELETE"] )
+# @crossdomain(origin='*',  methods=["DELETE"])
+def delete_topic( topic ):
+	pass
+
+
+'''
+Handles GET request
+'''
+@app.route( "/api/resource/<resource>", methods=["GET"] )
+# @crossdomain(origin='*',  methods=["GET"])
+def get_topic( topic ):
+	pass
+
+
+'''
+Handles OPTIONS request
+'''
+@app.route( "/api/resource/<resource>", methods=["OPTIONS"] )
+# @crossdomain(origin='*',  methods=["OPTIONS"])
+def options_topic( topic ):
+	pass
+
+
+'''
+Handles POST request
+'''
+@app.route( "/api/resource/<resource>", methods=["POST"] )
+# @crossdomain(origin='*',  methods=["POST"])
+def post_topic( topic ):
+	pass
+
+
+'''
+Handles PUT request
+'''
+@app.route( "/api/resource/<resource>", methods=["PUT"] )
+# @crossdomain(origin='*',  methods=["PUT"])
+def update_topic( topic ):
+	pass
+
+
+
+# Launchlist 
+'''
+Handles DELETE request
+'''
+@app.route( "/api/launchlist/<launchlist>", methods=["DELETE"] )
+# @crossdomain(origin='*',  methods=["DELETE"])
+def delete_topic( topic ):
+	pass
+
+
+'''
+Handles GET request
+'''
+@app.route( "/api/launchlist/<launchlist>", methods=["GET"] )
+# @crossdomain(origin='*',  methods=["GET"])
+def get_topic( topic ):
+	pass
+
+
+'''
+Handles OPTIONS request
+'''
+@app.route( "/api/launchlist/<launchlist>", methods=["OPTIONS"] )
+# @crossdomain(origin='*',  methods=["OPTIONS"])
+def options_topic( topic ):
+	pass
+
+
+'''
+Handles POST request
+'''
+@app.route( "/api/launchlist/<launchlist>", methods=["POST"] )
+# @crossdomain(origin='*',  methods=["POST"])
+def post_topic( topic ):
+	pass
+
+
+'''
+Handles PUT request
+'''
+@app.route( "/api/launchlist/<launchlist>", methods=["PUT"] )
+# @crossdomain(origin='*',  methods=["PUT"])
+def update_topic( topic ):
+	pass
+
+
+
+# Account 
+'''
+Handles DELETE request
+'''
+@app.route( "/api/account/<account>", methods=["DELETE"] )
+# @crossdomain(origin='*',  methods=["DELETE"])
+def delete_topic( topic ):
+	pass
+
+
+'''
+Handles GET request
+'''
+@app.route( "/api/account/<account>", methods=["GET"] )
+# @crossdomain(origin='*',  methods=["GET"])
+def get_topic( topic ):
+	pass
+
+
+'''
+Handles OPTIONS request
+'''
+@app.route( "/api/account/<account>", methods=["OPTIONS"] )
+# @crossdomain(origin='*',  methods=["OPTIONS"])
+def options_topic( topic ):
+	pass
+
+
+'''
+Handles POST request
+'''
+@app.route( "/api/account/<account>", methods=["POST"] )
+# @crossdomain(origin='*',  methods=["POST"])
+def post_topic( topic ):
+	pass
+
+
+'''
+Handles PUT request
+'''
+@app.route( "/api/account/<account>", methods=["PUT"] )
+# @crossdomain(origin='*',  methods=["PUT"])
+def update_topic( topic ):
+	pass
+
+
--- models.py
+++ models.py
@@ -1,51 +1,230 @@
-from google.appengine.ext import db
+from google.appengine.ext import ndb
+# from google.appengine.api import search
 
-class Topic( db.Model ):
-    name = db.StringProperty( default="Psuedoscience", multiline=False, required=True )
-    discription = db.TextProperty( default="It's real!!!", required=True )
+
+# '''
+# Returns the index the document is saved under
+# '''
+# def get_document_index( document ):
+#     # if isinstance( document, TopicModel ):
+#     return document.__name__
+
+# '''
+# Returns a document by its id
+# @param [string] id - the id of document
+# @param [string] index_name - the name of Search Index
+# to search in
+# @return [object] document - the document object, needs to be cleaned
+# to be accessible as a dictionary
+# '''
+# def get_document_by_id( id, index_name ):
+#     index = search.Index( name=index_name )
+#     # TODO: wrap this in a try catch
+#     return index.get( id )
+
+
+#  '''
+# Need to send cursor string from results.cursor to client, if it exists
+# cursor_string = cursor.web_safe_string
+# '''
+# def query_index( query_string, index_name, query_options=None ):
+#     index = search.Index( name=index_name )
+#     results = []
+#     if query_options is None:
+#         query_options = create_query_options()
+#     query = search.Query(
+#         query_string=query_string,
+#         options=query_options
+#         )
+#     try:
+#         results = index.search( query )
+#     except search.Error:
+#         logging.exception( "Query failed" )
+#     return results
+
+
+# '''
+#     Sets the query options like...
+#     which fields should returned items have,
+#     if there should be a cursor to keep track of where in the query
+#     the user is
+# '''
+# def create_query_options( returned_fields, cursor=None, result_limit=10 ):
+#     if cursor is None:
+#         cursor = search.Cursor()
+#     query_options = search.QueryOptions(
+#         limit=result_limit,
+#         returned_fields=returned_fields,
+#         cursor=cursor
+#         )
+#     return query_options
+
+
+# '''
+# need to send query string to client to retrieve past query
+# '''
+# def create_query_string( query_dict ):
+#     query_string = ""
+#     for key in query_dict.keys():
+#         if query_dict[key] == "" or query_dict[key] == None:
+#             continue
+#         query_string += " " + key + "=" + str( query_dict[key] )
+#     return query_string
+
+
+# '''
+# '''
+# def retrieve_query( query_string, cursor_string, index_name ):
+#     index = search.Index( name=index_name )
+#     cursor = search.Cursor( cursor_string )
+#     query_options = search.QueryOptions( cursor=cursor, limit=10 )
+#     query = search.Query( query_string=query_string,
+#                             options=query_options )
+#     return index.search( query )
+
+
+# '''
+# inserts document into index, saving it in the process
+# NOTE:
+# You can pass up to 200 documents at a time to the put() method.
+# Batching puts is more efficient than adding documents one at a time.
+# '''
+# def save_document( document, index_name ):
+#     index = search.Index( name=index_name )
+#     try:
+#         index.put( document )
+#     except search.Error:
+#         logging.exception( "Failed to save document" )
+
+
+# def update_document( document ):
+#     pass
+
+
+# def delete_document( document ):
+#     pass
+
+
+##################################################
+# Models
+##################################################
+class TopicModel( ndb.Model ):
+    name = ndb.StringProperty( default="Psuedoscience", multiline=False, required=True )
+
+    description = ndb.TextProperty( default="It's real!!!", required=True )
+    
+    icon = ndb.LinkProperty( required=True )
+    
     # display this topic on the front page?
-    display_front_page = db.BooleanProperty( default=False )
-    # list of SubTopics that under this Topic, only holds refrences
-    # empty list by default
-    sub_topics = db.ListProperty( item_type=db.ReferenceProperty )
-    num_sub_topics = db.IntegerProperty( default=0 )
+    display_front_page = ndb.BooleanProperty( default=False )
+    
+    # this is a list of sub_topic keys
+    sub_topics = ndb.KeyProperty( repeated=True )
+    
+    # num_sub_topics = ndb.IntegerProperty( default=0 )
+
+    
+    def update( self, name=None, description=None, icon=None, sub_topics=None  ):
+        if name != None and name != self.name:
+            self.name = name
+    
+        if description != None and description != self.description:
+            self.description = description
+    
+        if icon != None and icon != self.icon:
+            self.icon = icon
+    
+        if sub_topics != None and sub_topics != self.sub_topics:
+            self.sub_topics = sub_topics
+    
+        self.put()
+
+    
+    def add_sub_topic( self, sub_topic )
+    
+        if not isinstance( sub_topic, ndb.KeyProperty ):
+            return False
+    
+        if not isinstance( get_document_by_id( sub_topic ) ):
+            return False
+    
+        self.sub_topics.append( sub_topic )
+    
+        self.put()
+
+    
+    @classmethod
+    def _get_index( cls ):
+        return "TopicModel"
 
 
-class SubTopic( db.Model ):
-    name = db.StringProperty( default="Phrenology", multiline=False, required=True )
-    discription = db.TextProperty( default="Can you feel me?", required=True )
-    # list of Topics that this SubTopic belongs to
-    # only holds refrences, empty list by default
-    topics = db.ListProperty( item_type=db.ReferenceProperty )
-    num_topics = db.IntegerProperty( default=1 )
-    # list of Tutorials that belong to this SubTopic
-    # only holds refrences, empty list by default
-    tutorials = db.ListProperty( item_type=db.ReferenceProperty )
-    num_tutorials = db.IntegerProperty( default=0 )
+
+# class SubTopicModel( ndb.Model ):
+#     name = ndb.StringProperty( default="Phrenology", multiline=False, required=True )
+    
+#     description = ndb.TextProperty( default="Can you feel me?", required=True )
+    
+#     # list of topic keys
+#     topics = ndb.KeyProperty( repeated=True )
+    
+#     num_topics = ndb.IntegerProperty( default=1 )
+    
+#     # list of launchlist keys 
+#     launchlist = ndb.KeyProperty( repeated=True )
+#     # num_launchlist = ndb.IntegerProperty( default=0 )
 
 
-class Tutorial( db.Model ):
-    name = db.StringProperty( default="How to fleece rubes?", required=True )
-    discription = db.TextProperty( default="You will learn.", required=True )
-    # list of Resoruce that belong to this Tutorial
-    # only holds refrences, empty list by default
-    resoruce = db.ListProperty( item_type=db.ReferenceProperty )
-    num_resoruce = db.IntegerProperty( default=0 )
-    rating = db.RatingProperty()
+# class LaunchListModel( ndb.Model ):
+#     name = ndb.StringProperty( default="How to fleece rubes?", required=True )
+#     description = ndb.TextProperty( default="You will learn.", required=True )
+#     # list of Resoruce that belong to this LaunchList
+#     # only holds refrences, empty list by default
+#     resoruce = ndb.ListProperty( item_type=ndb.KeyProperty )
+#     # num_resoruce = ndb.IntegerProperty( default=0 )
+#     # rating = ndb.RatingProperty()
+#     # the user that added to this tutorial
+#     contributor = ndb.KeyProperty()
+#     rating = ndb.StringProperty()
 
 
-class Resoruce( db.Model ):
-    name = db.StringProperty( default="How to fleece rubes?", required=True )
-    discription = db.TextProperty( default="You will learn.", required=True )
-    link = db.LinkProperty( default="mta.io/#", required=True)
-    # resource_type = db.ReferenceProperty()
-    resource_type = db.StringProperty( default="Text", required=True )
-    rating = db.RatingProperty()
+# class ResourceModel( ndb.Model ):
+#     name = ndb.StringProperty( default="How to fleece rubes?", required=True )
+#     description = ndb.TextProperty( default="You will learn.", required=True )
+#     link = ndb.LinkProperty( default="mta.io/#", required=True)
+#     # resource_type = ndb.KeyProperty()
+#     resource_type = ndb.StringProperty( default="Text", required=True )
+#     # rating = ndb.RatingProperty()
+#     source_author = ndb.StringProperty()
+#     source_date = ndb.DateProperty()
+#     # the user that added to this resource
+#     contributor = ndb.KeyProperty()
+#     # list of LaunchLists that this Resource belong to
+#     # only holds refrences, empty list by default
+#     launch_list = ndb.ListProperty( item_type=ndb.KeyProperty )
+#     # num_launch_list = ndb.IntegerProperty( default=0 )
+#     rating_smily = ndb.StringProperty()
 
 
-class Account( db.Model ):
-    first_name = db.StringProperty( default="Friedrich", required=True )
-    last_name = db.StringProperty( default="Nietzsche", required=True )
-    discription = db.TextProperty( default="Iam the UberMentch!", required=True )
-    # "A user with a Google account"
-    user = UserProperty( auto_current_user=True, auto_current_user_add=True, required=True )
+# class AccountModel( ndb.Model ):
+#     first_name = ndb.StringProperty( default="Friedrich", required=True )
+#     last_name = ndb.StringProperty( default="Nietzsche", required=True )
+#     profile_type = ndb.StringProperty( required=True )
+#     # needs to be validated
+#     email = ndb.EmailProperty( required=True )
+#     description = ndb.TextProperty( default="Iam the UberMentch!", required=True )
+#     # "A user with a Google account"
+#     user = UserProperty( auto_current_user=True, auto_current_user_add=True, required=True )
+#     # list of LaunchLists that this Account Created
+#     # only holds refrences, empty list by default
+#     launch_list = ndb.ListProperty( item_type=ndb.KeyProperty )
+#     # list of LaunchLists that this Account Created
+#     resources = ndb.ListProperty( item_type=ndb.KeyProperty )
+
+
+# class SubscriberModel( ndb.Model ):
+#     first_name = ndb.StringProperty( default="Friedrich" )
+#     last_name = ndb.StringProperty( default="Nietzsche" )
+#     # needs to be validated
+#     email = ndb.EmailProperty( required=True )
+    
+
