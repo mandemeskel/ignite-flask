d8a0cfa5fb3fae8562632c4f52d0b4de68e0e24b
Michael T. Andemeskel <mtandemeskel@gmail.com>
2016-08-01 02:07:11 -0700

    Overhaul of API and database models
    Added Flask-RESTful
    Implemented RestModel and RestApi super classes
    Implemented Topic, TopicApi , and TopicsApi subclasses

---
+++ rest_model.py
@@ -0,0 +1,295 @@
+# the basics
+import logging
+import json
+from datetime import date
+
+# Google stuff
+from google.appengine.ext import ndb
+
+# Import the Flask Framework
+from flask import Flask, render_template, request, Response
+from flask_restful import Resource, Api
+
+# Import to allow crossdomain requests
+from crossdomain import crossdomain
+
+app = Flask( __name__ )
+api = Api( app )
+# TODO: turn app.debug and developing off when launching
+app.debug = True
+DEVELOPING = True
+
+
+
+class RESPONSE_STATUS(): 
+    failed = False
+    success = True
+    retry = "retry"
+
+
+
+# TODO: needed?
+def check_key( urlsafe_key, cls, return_model = False ):
+    model = ndb.Key( urlsafe = urlsafe_key ).get()
+    
+    # make sure we are editing the right type of model
+    if not isinstance( model, cls ):
+        return False
+
+    if return_model:
+        if model is None:
+            return False
+
+        return model
+
+    return True
+
+
+
+class RestModel( ndb.Model ):
+
+    name = ndb.StringProperty( default="Psuedoscience", required=True )
+    description = ndb.TextProperty( default="It's real!!!" )
+    contributors = ndb.KeyProperty( repeated=True )
+    date_created = ndb.DateProperty( auto_now_add=True )
+    last_update = ndb.DateProperty( auto_now=True )
+    OBJECT_PROPS = [ "contributors", "date_created", "last_update" ]
+
+
+    # Checks to see if key is an actual entity key 
+    @classmethod
+    def check_key( cls, urlsafe_key, return_model = False ):
+        model = ndb.Key( urlsafe = urlsafe_key ).get()
+
+        # make sure we are editing the right type of model
+        if not isinstance( model, cls ):
+            return False
+
+        if return_model:
+            if model is None:
+                return False
+
+            return model
+
+        return True
+
+
+    # Convert dict to model
+    @classmethod
+    def dict_to_model( cls, model ):
+        pass
+
+
+    # Turns model into json encodable dict
+    def to_dict( self, object_props=[], includes=[], excludes=[] ):
+        if includes != []:
+            dct = super( RestModel, self ).to_dict( include=includes, exclude=excludes )
+        else:
+            dct = super( RestModel, self ).to_dict( exclude=excludes )
+
+        if object_props == []:
+            object_props = dct.keys()
+        # if all the key lists are excluded from the model's dict
+        # then we don't need to do anything
+        elif object_props == excludes:
+            # added this in two places for optimization, one less key/value to look up
+            # by default to_dict does not include the model's key
+            dct[ "key" ] = self.key.urlsafe()
+            return dct
+
+        # to_dict does not convert objects properties i.e. datetime, keys etc.
+        for prop in object_props:
+            if prop not in dct:
+                continue
+
+            a_obj = dct[ prop ]
+
+            if isinstance( a_obj, list ):
+                if a_obj == []:
+                    continue
+
+                if not isinstance( a_list[0], ndb.KeyProperty ):
+                    continue
+
+                urlsafe_keys = []
+                # convert keys into urlsafe strings
+                for model_key in a_obj:
+                    urlsafe_keys.append( model_key.urlsafe() )
+
+                dct[ prop ] = urlsafe_keys
+
+            elif isinstance( a_obj, date ):
+                # TODO: add total secounds since 1972 for comparison, frontend it?
+                dct[ prop ] = { 
+                    "year": a_obj.year,
+                    "month": a_obj.month,
+                    "day": a_obj.day
+                }
+
+            elif isinstance( a_obj, ndb.KeyProperty ):
+                dct[ prop ] = a_obj.urlsafe()
+
+        # added this in two places for optimization, one less key/value to look up
+        # by default to_dict does not include the model's key
+        dct[ "key" ] = self.key.urlsafe()
+
+        return dct
+
+
+    # Creates an instance of the class and saves to database
+    # returns the model
+    @classmethod
+    def create( cls, data ):
+        model = cls( 
+            name=data[ "name" ],
+            description=data.get( "description", "" ),
+            contributors=data.get( "contributors", [] )
+        )
+
+        try:
+            model.put()
+        except Exception, e:
+            return False
+        
+        return model.key.urlsafe()
+
+
+    # A hook that is called before delete()
+    @classmethod
+    def _pre_delete_hook( cls, key ):
+        pass
+
+
+    # Creates dummy data for model
+    def create_dummy_data( self ):
+        pass
+
+
+    # Delete model from database
+    def delete( self ):
+        try:
+            self.key.delete()
+        except Exception, e:
+            return False
+        else:
+            return True
+            
+
+    # Updates the model with the data passed
+    # returns boolean depedent on the success of the update
+    def update( self, data ):
+        model_name = data.get( "name", self.name )
+        model_descr = data.get( "description", self.description )
+        contributors = data.get( "contributors", self.contributors )
+        
+        if model_name != self.name:
+            self.name = model_name
+
+        if model_descr != self.description:
+            self.description = model_descr
+
+        if contributors is not self.contributors:
+            self.contributors = contributors
+
+        try:
+            self.put()
+        except Exception, e:
+            return False
+        else:
+            return self.key.urlsafe()
+
+
+    # Checks to see if model is in the passed list 
+    def is_in_list( self, a_list ):
+        return ( self in a_list )
+
+
+    # Checks to see if the model's is in the passed list
+    def is_key_in_list( self, a_list ):
+        return ( self.key in a_list )
+
+
+
+class RestApi( Resource ):
+
+    model_class = RestModel
+
+    # Deletes the model from database
+    @classmethod
+    def delete( cls, urlsafe_key ):
+        model = cls.model_class.check_key( urlsafe_key, return_model = True )
+
+        if model is False:
+            return { "status": False }, 400
+
+        if model.delete() is False:
+            return { "status":  False }, 500
+        
+        return { "status": True }
+
+
+    # Retrives the model and sends it as a dict
+    @classmethod
+    def get( cls, urlsafe_key ):
+        data = request.form
+
+        if DEVELOPING:
+            logging.log( logging.INFO, data )
+
+        model = cls.model_class.check_key( urlsafe_key, return_model = True )
+
+        if model is False:
+            return { "status": False }, 400
+
+        model_dict = model.to_dict()
+        return { "status": True, "model": model_dict }
+
+
+    # Updates the model with the data passed
+    @classmethod
+    def post( cls, urlsafe_key ):
+        data = request.form
+
+        if DEVELOPING:
+            logging.log( logging.INFO, data )
+
+        model = cls.model_class.check_key( urlsafe_key, return_model = True )
+
+        if model is False:
+            return { "status": False }, 400
+
+        urlsafe_key = model.update( data )
+
+        if urlsafe_key:
+            return { "status": True, "key": urlsafe_key }
+        else:
+            return { "status": False }, 500
+
+
+    # Creates a model from data passed, returns model's key
+    @classmethod
+    def put( cls ):
+        data = request.form
+
+        if DEVELOPING:
+            logging.log( logging.INFO, data )
+
+        if "name" not in data or "description" not in data:
+            return { "status": False }, 400
+
+        urlsafe_key = cls.model_class.create( data )
+
+        if urlsafe_key is False or urlsafe_key is None:
+            return { "status": False }, 500
+        else:
+            return { "status": True, "key": urlsafe_key }, 201
+
+
+
+# requests that will be routed to the RestModel class
+api.add_resource( RestApi, 
+    "/test/<urlsafe_key>",
+    "/test" 
+)
+
+
---
+++ topic_model.py
@@ -0,0 +1,329 @@
+# the basics
+import logging
+import json
+
+# Google stuff
+from google.appengine.ext import ndb
+
+# Import the Flask Framework
+from flask import Flask, render_template, request, Response
+from flask_restful import Resource, Api
+
+# Import to allow crossdomain requests
+from crossdomain import crossdomain
+
+# Our stuff
+from rest_model import RestApi, RestModel, RESPONSE_STATUS
+
+app = Flask( __name__ )
+api = Api( app )
+# TODO: turn app.debug and developing off when launching
+app.debug = True
+DEVELOPING = True
+
+
+
+class Topic( RestModel ):
+
+    # TODO: do not allow multiline names
+    # inherited from RestModel
+    # name = ndb.StringProperty( default="Psuedoscience", required=True )
+    # description = ndb.TextProperty( default="It's real!!!" )
+    # contributors = ndb.KeyProperty( repeated=True )
+    # date_created = ndb.DateProperty( auto_now_add=True )
+    # last_update = ndb.DateProperty( auto_now=True )
+    icon = ndb.StringProperty( required=True )
+    # display this topic on the front page?
+    display_front_page = ndb.BooleanProperty( default=False )
+    # this is a list of launchlists keys
+    launchlists = ndb.KeyProperty( repeated=True )
+    num_launchlists = ndb.IntegerProperty( default=0 )
+    REQUIRED_PROPERTIES = [ "name", "description", "icon" ]
+    OTHER_PROPERTIES = [ "display_front_page", "launchlists", "num_launchlists" ]
+    OBJECT_PROPS = RestModel.OBJECT_PROPS
+    OBJECT_PROPS.append( "launchlists" )
+
+    # Returns a list of Topics with display_front_page=True
+    # @classmethod
+    # def get_front_page_topics( cls, num_topics=3, 
+    #     excludes=[ "launchlists", "display_front_page" ] ):
+    #     return
+    
+
+    # Creates an instance of the class and saves to database
+    # returns the model
+    @classmethod
+    def create( cls, data, return_as_dict=True ):
+        # check that required properties are in the passed data
+        for prop in cls.REQUIRED_PROPERTIES:
+            if prop not in data:
+                logging.log( logging.ERROR, "Topic.create(), miss property: " + prop )
+                return False
+
+        # TODO: validate passed data
+        # for prop in data.keys():
+        #     if prop == "name":
+        #         if prop is not str:
+        #             logging.log( "logging.ERROR", "Topic.create(), bad value: " + prop )
+        #             return False
+
+        if DEVELOPING:
+            logging.log( logging.INFO, data )
+
+        topic = cls( 
+            name=data[ "name" ],
+            description=data[ "description" ],
+            icon=data[ "icon" ],
+            display_front_page=data[ "display_front_page" ]
+        )
+
+        try:
+            topic.put()
+        except Exception as e:
+            logging.log( logging.ERROR, "Topic.create(), failed to create Topic: " + topic.name )
+            logging.log( logging.ERROR, e )
+            return False
+
+        if return_as_dict:
+            return topic.to_dict()
+
+        return topic
+
+
+    # TODO: move out to parent class?
+    # Creates the first set of dummy topics
+    @classmethod
+    def create_dummy_topics( cls ):
+        topics = []
+        names = [ "Music", "Design", "Code", "Politics", "Life", "Relationships", "Health", "School", "Random" ]
+        description = '"Happiness today is just a song away, just a song\nI love your music, baby" \n- Just Like Music, Erick Sermon feat. Marvin Gaye'
+        icon = "../icons/apple_music_icon_trns.png"
+        display_front_page = True
+       
+        for name in names:
+            topic = cls.create( { 
+                "name": name,
+                "description": description,
+                "icon": icon,
+                "display_front_page": display_front_page
+            } )
+
+            if Topic is False:
+                continue
+
+            topics.append( topic )
+
+        return topics
+
+
+    # Returns a list of topics ordered by number of launchlists they contain
+    @classmethod
+    def get_topics( cls, num_topics=3, includes=[], excludes=[] ):
+        # TODO: make query an dynamic i.e. an arg
+        query = cls.query( cls.display_front_page == True ).order( -cls.num_launchlists )
+
+        # search for topics matching our query params
+        try:
+            topics = query.fetch( num_topics )
+        except Exception as e:
+            logging.log( logging.ERROR, "Topic.get_topics(), failed to fetch query" )
+            logging.log( logging.ERROR, e )
+            return False
+
+        # if DEVELOPING:
+        #     logging.log( logging.INFO, topics )
+
+        if topics == [] or topics is None:
+            return cls.create_dummy_topics()
+
+        # convert all topics into dicts
+        topics_dict = []
+        for topic in topics:
+            topics_dict.append( topic.to_dict( includes=includes, excludes=excludes ) )
+
+        return topics_dict
+
+
+    # Adds launchlist to Topic
+    def add_launchlist( self, launchlist_urlsafe_key ):
+        launchlist = cls.check_key( launchlist_urlsafe_key, return_model=True )
+        if launchlist.key not in self.launchlists:
+            self.launchlists.append( launchlist.key )
+            self.num_launchlists += 1
+            return True
+        return False
+
+
+    # Creates dummy data for model
+    def create_dummy_data( self ):
+        pass
+
+
+    # Removes launchlist from Topic
+    def remove_launchlist( self, launchlist_urlsafe_key ):
+        # TODO: create method to transform websafe_key to regular key
+        launchlist = cls.check_key( launchlist_urlsafe_key, return_model=True )
+
+        if self.num_launchlists == 0:
+            return False
+        elif launchlist.key in self.launchlists:
+            self.launchlists.remove( launchlist.key )
+            self.num_launchlists -= 1
+            return True
+        
+        return False
+
+
+    # Turns model into json encodable dict
+    def to_dict( self, includes=None, excludes=None ):        
+        # if DEVELOPING:
+        #     logging.log( logging.INFO, type( self ) )
+
+        if includes is not None:
+            dct = super( Topic, self ).to_dict( 
+                    object_props=Topic.OBJECT_PROPS, 
+                    includes=includes, 
+                    excludes=excludes 
+                )
+        else:
+            dct = super( Topic, self ).to_dict( 
+                    object_props=Topic.OBJECT_PROPS, 
+                    excludes=excludes 
+                )
+        return dct
+
+
+    '''
+        UPDATE SCENARIOS
+        update regular properties like name, descrp, icon, etc.
+        update lists by sending list item that with DELETE or POST argument
+        in other words lists belonging to this Topic will not be edited using the POST
+        method of this Topic?
+    '''
+    # TODO: validate passed data
+    # Update this Topics data and return the Topic 
+    def update( self, data ):
+        model_dict = self.to_dict();
+        for prop in data.keys():
+            if prop not in Topic.REQUIRED_PROPERTIES or prop not in Topic.OTHER_PROPERTIES:
+                continue
+            if data[ prop ] == model_dict[ prop ]:
+                continue
+            model_dict[ prop ] == data[ prop ]
+        
+        # TODO: create dict_to_model?
+        model = Topic.dict_to_model( model_dict )
+        try:
+            model.put()
+        except Exception as e:
+            logging.log( "logging.ERROR", "Topic.update(), failed to update Topic: " + str( data ) )
+            logging.log( "logging.ERROR", e )
+            return False
+
+        return model_dict
+
+
+
+class TopicApi( RestApi ):
+    model_class = Topic
+
+    # TODO: needs auth to delete
+    # Handles all behaviour that removes data from this Topic
+    @classmethod
+    def delete( cls, topic_ulrsafe_key, list_urlsafe_key=None ):
+        # if list_urlsafe_key != None:
+        #     topic = cls.model_class.check_key( topic_ulrsafe_key, return_model=True )
+        #     result = cls.model_class.remove_launchlist( list_urlsafe_key )
+            
+        #     if result:
+        #         return { "status": False, "msg": "failed to remove launchlist" }, 500
+
+        #     return { "status": True }
+        # else:
+        #     return super( TopicApi, cls ).delete( topic_ulrsafe_key )
+
+        return { "status": False, "msg": "no perms" }, 403
+
+
+    # Updates the topic with the data passed
+    @classmethod
+    def post( cls, urlsafe_key ):
+        return { "status": False, "msg": "no perms" }, 403
+
+
+    # Creates a model from data passed, returns model's key
+    @classmethod
+    def put( cls ):
+        return { "status": False, "msg": "no perms" }, 403
+
+
+
+class TopicsApi( RestApi ):
+    model_class = Topic
+
+    # TODO: validate data
+    # TODO: remove includes and excludes params
+    # gets multiple topics
+    @classmethod
+    def get( cls, ajax=False, num=3, includes=[], excludes=[] ):
+        # NOTE: not python 3.x friendly
+        if includes != [] and isinstance( includes, basestring ):
+            includes = includes.split( "," )
+
+        # NOTE: not python 3.x friendly
+        if excludes != [] and isinstance( excludes, basestring ):
+            excludes = excludes.split( "," )     
+
+        if DEVELOPING:
+            logging.log( logging.INFO, type( includes ) )
+            logging.log( logging.INFO, type( excludes ) )
+
+        # send only the neccassary info by default
+        if excludes == []:
+            excludes = cls.model_class.OBJECT_PROPS
+            excludes.extend( [ "display_front_page", "num_launchlists" ] )
+
+        # NOTE: not python 3.x friendly, should be isinstance( num, str )
+        if isinstance( num, basestring ):
+            try:
+                num = int( num )
+            except Exception, e:
+                return { "status": False }, 500
+
+
+        # if DEVELOPING:
+        #     logging.log( logging.INFO, num )
+
+        if ajax:
+            topics = cls.model_class.get_topics( 
+                num_topics=num, 
+                includes=includes, 
+                excludes=excludes 
+            )
+
+            if topics is False:
+                return { "status": False }, 500
+            elif topics is None:
+                return { "status": False, RESPONSE_STATUS.retry: True }, 204
+
+            return { "status": True, "topics": topics }
+
+        else:
+            return { "status": False }, 404
+
+
+
+# requests that will be routed to the RestModel class
+api.add_resource( TopicApi,
+    "/topic/<urlsafe_key>",
+    "/topic"
+)
+
+# TODO: remove includes and excludes url params
+api.add_resource( TopicsApi, 
+    "/topics/<ajax>/<num>/<includes>/<excludes>",
+    "/topics/<ajax>/<num>/<includes>",
+    "/topics/<ajax>/<num>",
+    "/topics/<ajax>",
+    "/topics" 
+)
